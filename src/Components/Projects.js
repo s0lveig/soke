b27dc811d962bc73f4cb7f6389bc3eb07aeb5f05
Solveig Knutsen <hei@solveigknutsen.no>
2020-01-28 20:53:02 +0100

    edit page title and mobile css

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -15,22 +15,22 @@ const Projects = () => {
                 </div>
             </div>
 
-            <div className="row justify-content-center">
-                <div className="col-4 pr-5">
+            <div className="row justify-content-center p-2">
+                <div className="col-sm-4 pt-2">
                     <img className="img-fluid" src="./images/soke_project_1.png" />
                 </div>
-                <div className="col-4 pl-5">
+                <div className="col-sm-4 pt-2">
                     <img className="img-fluid" src="./images/soke_project_2.png" />
                 </div>
             </div>
 
             <div className="row justify-content-center">
-                <div className="col-4 pr-5 pt-2">
+                <div className="col-sm-4">
                     <p>Aoı Project is about making sustainable everyday objects and garments in an ethical way,
                     to replace consumer goods and add to a more sustainable wardrobe and lifestyle.</p>
                     <p><a className="text-reset case-link" href="http://www.aoiproject.eu" target="_blank">www.aoiproject.eu</a></p>
                 </div>
-                <div className="col-4 pl-5 pt-2">
+                <div className="col-sm-4">
                     <p>Good Type Foundry is an award winning Oslo based type foundry, offering retail
                         and custom typefaces for both digital and printed use.
                         We design, develop and publish contemporary typefaces with high attention to detail.
